6273172b5457017af832de89e516c0e414eb71c3
David Hewitt <david@hewitt.tech>
2015-09-20 22:51:47 +0100

    Succeeded in getting the fullOptJS stage working. No .sjsir files yet.

--- src/main/scala/org/gradle/ScalaJSOptimizerTask.scala
+++ src/main/scala/org/gradle/ScalaJSOptimizerTask.scala
@@ -1,13 +1,24 @@
 package org.gradle
 
+import org.scalajs.core.tools.classpath.builder.PartialClasspathBuilder
+import org.scalajs.core.tools.io._
 import org.scalajs.core.tools.javascript.OutputMode
+import org.scalajs.core.tools.optimizer.ScalaJSOptimizer.Config
 import org.scalajs.core.tools.sem.Semantics
 import org.scalajs.core.tools.optimizer._
+import org.scalajs.core.tools.logging.ScalaConsoleLogger
 
 import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks._
+
+import collection.JavaConversions._
+
+import java.io._
 
 class ScalaJSOptimizerTask extends DefaultTask {
+
+  @OutputDirectory
+  var outputDir = new File(getProject.getBuildDir, "fastopt")
 
   @TaskAction
   def OptJS(): Unit = {
@@ -17,39 +28,62 @@ class ScalaJSOptimizerTask extends DefaultTask {
       OutputMode.ECMAScript51Isolated,
       ParIncOptimizer.factory
     )
-    println("Done.")
 
-    //  val s = streams.value
-    //    val output = (artifactPath in fullOptJS).value
-    //    val taskCache =
-    //            WritableFileVirtualTextFile(s.cacheDirectory / "fullopt-js")
-    //
-    //    IO.createDirectory(output.getParentFile)
-    //
-    //    val relSourceMapBase =
-    //    if ((relativeSourceMaps in fullOptJS).value)
-    //        Some(output.getParentFile.toURI())
-    //    else None
-    //
-    //    val opts = (scalaJSOptimizerOptions in fullOptJS).value
-    //
-    //
-    //    val outCP = new ScalaJSClosureOptimizer().optimizeCP(
-    //            new scalaJSOptimizer(),
-    //            (scalaJSPreLinkClasspath in fullOptJS).value,
-    //            Config(AtomicWritableFileVirtualJSFile(output))
-    //                    .withCache(Some(taskCache))
-    //                    .withWantSourceMap((emitSourceMaps in fullOptJS).value)
-    //                    .withRelativizeSourceMapBase(relSourceMapBase)
-    //                    .withBypassLinkingErrors(opts.bypassLinkingErrors)
-    //                    .withCheckIR(opts.checkScalaJSIR)
-    //                    .withDisableOptimizer(opts.disableOptimizer)
-    //                    .withBatchMode(opts.batchMode)
-    //                    .withCustomOutputWrapper(scalaJSOutputWrapper.value)
-    //                    .withPrettyPrint(opts.prettyPrintFullOptJS),
-    //            s.log)
-    //
-    //    Attributed.blank(output).put(scalaJSCompleteClasspath, outCP)
+    val cacheDir = new File(getProject.getBuildDir, "scalajs-cache")
+    val taskCache = WritableFileVirtualTextFile(new File(cacheDir, "fastopt-js"))
+
+    outputDir.mkdir()
+    cacheDir.mkdir()
+
+    val output = new File(outputDir, getProject.getName + "-fastopt.js")
+
+
+    println(System.getProperty("java.home"))
+
+//    val relSourceMapBase =
+//    if ((relativeSourceMaps in fullOptJS).value)
+//        Some(output.getParentFile.toURI())
+//    else None
+
+//    val opts = (scalaJSOptimizerOptions in fullOptJS).value
+
+    // Replaced above line with these
+    /** Whether to only warn if the linker has errors */
+    val bypassLinkingErrors: Boolean = false
+    /** Whether to run the optimizer in batch (i.e. non-incremental) mode */
+    val batchMode: Boolean = false
+    /** Whether to run the Scala.js optimizer */
+    val disableOptimizer: Boolean = false
+    /** Perform expensive checks of the sanity of the Scala.js IR */
+    val checkScalaJSIR: Boolean = true
+
+    val sourceSets = getProject.property("sourceSets").asInstanceOf[SourceSetContainer]
+
+    val cp = sourceSets.getByName("main").getRuntimeClasspath.getFiles
+    val pcp = PartialClasspathBuilder.build(collection.immutable.Seq[File](cp.toSeq: _*))
+//    val ccp = pcp.resolve(jsDependencyFilter.value, jsManifestFilter.value)
+    val ccp = pcp.resolve(identity, identity)
+
+    println(cp)
+    println(ccp.scalaJSIR)
+
+    val outCP = optimizer.optimizeCP(
+            ccp,
+            Config(AtomicWritableFileVirtualJSFile(output))
+              .withCache(Some(taskCache))
+              .withWantSourceMap(true)
+              .withRelativizeSourceMapBase(None)
+//              .withRelativizeSourceMapBase(relSourceMapBase)
+              .withBypassLinkingErrors(bypassLinkingErrors)
+              .withCheckIR(checkScalaJSIR)
+              .withDisableOptimizer(disableOptimizer)
+              .withBatchMode(batchMode)
+              .withCustomOutputWrapper(("", "")),
+            new ScalaConsoleLogger()
+            )
+
+    //Attributed.blank(output).put(scalaJSCompleteClasspath, outCP)
+    println("Done.")
   }
 
 }
